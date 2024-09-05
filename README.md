e53644976655a03ece4bafd5a7aa91b11f90179e
fgtrzah <admin@fgtrz.com>
2024-09-04 18:58:04 -0700

    feature: match colors - update readme

--- README.md
+++ README.md
@@ -2,3 +2,28 @@ first stab at porting spacecamp vim colorsheme to lua for
 use with neovim or lunarvim based distros.
 
 original: https://github.com/jaredgorski/SpaceCamp
+
+#### more about this port
+
+To create a full 1:1 translation of the spacecamp.vim color scheme to a Lua-based Neovim configuration, I started off using llm-gateway for a first draft of translation,
+and then included all relevant highlight groups and elements as defined in the original spacecamp.vim file.
+
+#### usage
+
+1. clone spacecamp.vim however you wish to `~/.vim/colors/spacecamp.vim`
+
+2. whether you're using lunarvim or not add:
+
+```lua
+-- ~/.config/lvim/config.lua
+lvim.plugins = {
+    ...
+    { "fgtrzah/spacecamp" },
+    ...
+}
+
+lvim.colorscheme = "spacecamp"
+vim.cmd("colorscheme spacecamp")
+
+-- ... the rest
+```
--- lua/spacecamp/init.lua
+++ lua/spacecamp/init.lua
@@ -1,102 +1,145 @@
-local M = {}
+-- gpt 4o, mistral, copilot, and ollama local gateway (trained on
+-- my personal system .cache directory) was utilized to create
+-- the initial draft of this port
+local colors = {
+  bg = "#1c1c1c",
+  fg = "#b2b2b2",
+  black = "#3a3a3a",
+  red = "#af5f5f",
+  green = "#87875f",
+  yellow = "#e4c9af",
+  blue = "#5f8787",
+  magenta = "#7e7e7e",
+  cyan = "#5f87af",
+  white = "#d0d0d0",
+  orange = "#ff8700",
+  pink = "#d75f87",
+  light_bg = "#262626",
+  light_fg = "#d0d0d0",
+  comment = "#6c6c6c",
+  none = "NONE"
+}
 
-function M.setup()
-  -- Define basic colors
-  local colors = {
-    bg = "#1e1e1e",          -- Background
-    fg = "#c5c8c6",          -- Foreground
-    red = "#cc6666",         -- Red
-    green = "#b5bd68",       -- Green
-    yellow = "#f0c674",      -- Yellow
-    blue = "#81a2be",        -- Blue
-    magenta = "#b294bb",     -- Magenta
-    cyan = "#8abeb7",        -- Cyan
-    white = "#ffffff",       -- White
-    black = "#000000",       -- Black
-    gray = "#7c7c7c",        -- Gray
+local apply_colorscheme = function()
+  local hl = vim.api.nvim_set_hl
+  local set_colors = {
+    -- General UI
+    Normal = { fg = colors.fg, bg = colors.bg },
+    Comment = { fg = colors.comment, italic = true },
+    CursorLine = { bg = colors.light_bg },
+    CursorLineNr = { fg = colors.yellow, bold = true },
+    LineNr = { fg = colors.comment },
+    VertSplit = { fg = colors.comment },
+    SignColumn = { bg = colors.bg },
+    StatusLine = { fg = colors.fg, bg = colors.light_bg },
+    StatusLineNC = { fg = colors.comment, bg = colors.bg },
+    Visual = { bg = colors.comment },
+    Pmenu = { fg = colors.fg, bg = colors.black },
+    PmenuSel = { fg = colors.black, bg = colors.yellow },
+    PmenuSbar = { bg = colors.black },
+    PmenuThumb = { bg = colors.light_fg },
+
+    -- Syntax highlighting
+    Identifier = { fg = colors.blue },
+    Function = { fg = colors.green },
+    Statement = { fg = colors.yellow },
+    Conditional = { fg = colors.red },
+    Repeat = { fg = colors.red },
+    Label = { fg = colors.yellow },
+    Operator = { fg = colors.orange },
+    Keyword = { fg = colors.magenta },
+    PreProc = { fg = colors.yellow },
+    Type = { fg = colors.cyan },
+    Constant = { fg = colors.orange },
+    String = { fg = colors.green },
+    Character = { fg = colors.green },
+    Number = { fg = colors.orange },
+    Boolean = { fg = colors.orange },
+    Float = { fg = colors.orange },
+    Special = { fg = colors.yellow },
+    SpecialChar = { fg = colors.orange },
+    SpecialComment = { fg = colors.comment },
+    Todo = { fg = colors.fg, bg = colors.yellow, bold = true },
+    Error = { fg = colors.red, bg = colors.none, bold = true },
+    WarningMsg = { fg = colors.orange, bg = colors.none, bold = true },
+    DiffAdd = { fg = colors.green, bg = colors.none },
+    DiffChange = { fg = colors.yellow, bg = colors.none },
+    DiffDelete = { fg = colors.red, bg = colors.none },
+    DiffText = { fg = colors.blue, bg = colors.none },
+
+    -- LSP
+    DiagnosticError = { fg = colors.red },
+    DiagnosticWarn = { fg = colors.yellow },
+    DiagnosticInfo = { fg = colors.blue },
+    DiagnosticHint = { fg = colors.cyan },
+    DiagnosticUnderlineError = { undercurl = true, sp = colors.red },
+    DiagnosticUnderlineWarn = { undercurl = true, sp = colors.yellow },
+    DiagnosticUnderlineInfo = { undercurl = true, sp = colors.blue },
+    DiagnosticUnderlineHint = { undercurl = true, sp = colors.cyan },
+
+    -- Treesitter
+    TSAnnotation = { fg = colors.yellow },
+    TSAttribute = { fg = colors.cyan },
+    TSBoolean = { fg = colors.orange },
+    TSCharacter = { fg = colors.green },
+    TSComment = { fg = colors.comment, italic = true },
+    TSConditional = { fg = colors.red },
+    TSConstant = { fg = colors.orange },
+    TSConstBuiltin = { fg = colors.orange },
+    TSConstMacro = { fg = colors.orange },
+    TSConstructor = { fg = colors.yellow },
+    TSError = { fg = colors.red },
+    TSException = { fg = colors.red },
+    TSField = { fg = colors.blue },
+    TSFloat = { fg = colors.orange },
+    TSFunction = { fg = colors.green },
+    TSFuncBuiltin = { fg = colors.green },
+    TSFuncMacro = { fg = colors.green },
+    TSInclude = { fg = colors.magenta },
+    TSKeyword = { fg = colors.magenta },
+    TSKeywordFunction = { fg = colors.magenta },
+    TSKeywordOperator = { fg = colors.magenta },
+    TSLabel = { fg = colors.yellow },
+    TSMethod = { fg = colors.green },
+    TSNamespace = { fg = colors.blue },
+    TSNone = { fg = colors.fg },
+    TSNumber = { fg = colors.orange },
+    TSOperator = { fg = colors.orange },
+    TSParameter = { fg = colors.blue },
+    TSParameterReference = { fg = colors.blue },
+    TSProperty = { fg = colors.blue },
+    TSPunctDelimiter = { fg = colors.fg },
+    TSPunctBracket = { fg = colors.fg },
+    TSPunctSpecial = { fg = colors.fg },
+    TSRepeat = { fg = colors.red },
+    TSString = { fg = colors.green },
+    TSStringRegex = { fg = colors.green },
+    TSStringEscape = { fg = colors.orange },
+    TSSymbol = { fg = colors.cyan },
+    TSTag = { fg = colors.yellow },
+    TSTagAttribute = { fg = colors.cyan },
+    TSTagDelimiter = { fg = colors.comment },
+    TSText = { fg = colors.fg },
+    TSTitle = { fg = colors.yellow, bold = true },
+    TSType = { fg = colors.cyan },
+    TSTypeBuiltin = { fg = colors.cyan },
+    TSVariable = { fg = colors.blue },
+    TSVariableBuiltin = { fg = colors.blue },
+
+    -- Git signs
+    GitSignsAdd = { fg = colors.green },
+    GitSignsChange = { fg = colors.yellow },
+    GitSignsDelete = { fg = colors.red },
   }
 
-  -- Editor
-  vim.cmd("highlight Normal guifg=" .. colors.fg .. " guibg=" .. colors.bg)
-  vim.cmd("highlight CursorLine guibg=" .. colors.black)
-  vim.cmd("highlight CursorColumn guibg=" .. colors.black)
-  vim.cmd("highlight LineNr guifg=" .. colors.gray)
-  vim.cmd("highlight CursorLineNr guifg=" .. colors.yellow)
-  vim.cmd("highlight VertSplit guifg=" .. colors.gray .. " guibg=" .. colors.bg)
-
-  -- Syntax Highlighting
-  vim.cmd("highlight Comment guifg=" .. colors.gray .. " gui=italic")
-  vim.cmd("highlight Constant guifg=" .. colors.cyan)
-  vim.cmd("highlight String guifg=" .. colors.green)
-  vim.cmd("highlight Character guifg=" .. colors.green)
-  vim.cmd("highlight Number guifg=" .. colors.yellow)
-  vim.cmd("highlight Boolean guifg=" .. colors.yellow)
-  vim.cmd("highlight Float guifg=" .. colors.yellow)
-  vim.cmd("highlight Identifier guifg=" .. colors.blue)
-  vim.cmd("highlight Function guifg=" .. colors.blue)
-  vim.cmd("highlight Statement guifg=" .. colors.red)
-  vim.cmd("highlight Conditional guifg=" .. colors.red)
-  vim.cmd("highlight Repeat guifg=" .. colors.red)
-  vim.cmd("highlight Label guifg=" .. colors.red)
-  vim.cmd("highlight Operator guifg=" .. colors.fg)
-  vim.cmd("highlight Keyword guifg=" .. colors.magenta)
-  vim.cmd("highlight Exception guifg=" .. colors.red)
-  vim.cmd("highlight PreProc guifg=" .. colors.magenta)
-  vim.cmd("highlight Include guifg=" .. colors.magenta)
-  vim.cmd("highlight Define guifg=" .. colors.magenta)
-  vim.cmd("highlight Macro guifg=" .. colors.magenta)
-  vim.cmd("highlight Type guifg=" .. colors.yellow)
-  vim.cmd("highlight StorageClass guifg=" .. colors.yellow)
-  vim.cmd("highlight Structure guifg=" .. colors.yellow)
-  vim.cmd("highlight Typedef guifg=" .. colors.yellow)
-  vim.cmd("highlight Special guifg=" .. colors.cyan)
-  vim.cmd("highlight SpecialChar guifg=" .. colors.cyan)
-  vim.cmd("highlight Tag guifg=" .. colors.blue)
-  vim.cmd("highlight Delimiter guifg=" .. colors.white)
-  vim.cmd("highlight SpecialComment guifg=" .. colors.gray)
-  vim.cmd("highlight Debug guifg=" .. colors.red)
-
-  -- UI Elements
-  vim.cmd("highlight Pmenu guibg=" .. colors.black .. " guifg=" .. colors.white)
-  vim.cmd("highlight PmenuSel guibg=" .. colors.blue .. " guifg=" .. colors.white)
-  vim.cmd("highlight PmenuSbar guibg=" .. colors.gray)
-  vim.cmd("highlight PmenuThumb guibg=" .. colors.white)
-  vim.cmd("highlight TabLine guibg=" .. colors.black .. " guifg=" .. colors.gray)
-  vim.cmd("highlight TabLineFill guibg=" .. colors.bg)
-  vim.cmd("highlight TabLineSel guibg=" .. colors.blue .. " guifg=" .. colors.white)
-
-  -- Diagnostics
-  vim.cmd("highlight ErrorMsg guifg=" .. colors.red .. " guibg=" .. colors.bg .. " gui=bold")
-  vim.cmd("highlight WarningMsg guifg=" .. colors.yellow .. " guibg=" .. colors.bg)
-  vim.cmd("highlight InfoMsg guifg=" .. colors.blue .. " guibg=" .. colors.bg)
-  vim.cmd("highlight HintMsg guifg=" .. colors.cyan .. " guibg=" .. colors.bg)
-  vim.cmd("highlight Error guifg=" .. colors.red .. " gui=bold")
-  vim.cmd("highlight Warning guifg=" .. colors.yellow)
-  vim.cmd("highlight Info guifg=" .. colors.blue)
-  vim.cmd("highlight Hint guifg=" .. colors.cyan)
-
-  -- Git
-  vim.cmd("highlight GitSignsAdd guifg=" .. colors.green)
-  vim.cmd("highlight GitSignsChange guifg=" .. colors.yellow)
-  vim.cmd("highlight GitSignsDelete guifg=" .. colors.red)
-
-  -- Search
-  vim.cmd("highlight Search guibg=" .. colors.yellow .. " guifg=" .. colors.bg)
-  vim.cmd("highlight IncSearch guibg=" .. colors.magenta .. " guifg=" .. colors.bg)
-  vim.cmd("highlight MatchParen guibg=" .. colors.gray .. " guifg=" .. colors.white)
-
-  -- Status Line
-  vim.cmd("highlight StatusLine guifg=" .. colors.white .. " guibg=" .. colors.gray)
-  vim.cmd("highlight StatusLineNC guifg=" .. colors.gray .. " guibg=" .. colors.black)
-
-  -- Visual Mode
-  vim.cmd("highlight Visual guibg=" .. colors.blue .. " guifg=" .. colors.white)
-
-  -- Diff
-  vim.cmd("highlight DiffAdd guibg=" .. colors.green .. " guifg=" .. colors.black)
-  vim.cmd("highlight DiffChange guibg=" .. colors.yellow .. " guifg=" .. colors.black)
-  vim.cmd("highlight DiffDelete guibg=" .. colors.red .. " guifg=" .. colors.black)
-  vim.cmd("highlight DiffText guibg=" .. colors.blue .. " guifg=" .. colors.black)
+  for group, color in pairs(set_colors) do
+    hl(0, group, color)
+  end
 end
 
-return M
+vim.o.background = "dark"
+vim.o.termguicolors = true
+
+apply_colorscheme()
+
+lvim.colorscheme = apply_colorscheme
